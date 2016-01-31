10415f9215408dad57b0961780908f143a6f579b
Giovanni Picardi <giovanni.picardi@gmail.com>
2016-01-31 03:51:25 +0100

    New, updated submission.

--- Analyses.r
+++ Analyses.r
@@ -1,6 +1,111 @@
 data(mtcars)
+colSums(is.na(mtcars))
+str(mtcars)
 
-pairs(mtcars)
+mc <- transform(mtcars, cyl=factor(cyl), vs=factor(vs),am=factor(am, labels=c("Automatic","Manual")),
+                gear=factor(gear), carb=factor(carb), wt=wt-mean(wt), disp=disp-mean(disp),
+                hp = hp-mean(hp))
+
+mc <- transform(mtcars, cyl=factor(cyl), vs=factor(vs),am=factor(am, labels=c("Automatic","Manual")),
+                gear=factor(gear), carb=factor(carb))
+
+
+pairs(mc, upper.panel=panel.smooth)
+
+fit1 <- lm(mpg ~ am, mc)
+summary(fit1)
+par(mfrow=c(2,2))
+plot(fit1)
+confint(fit1)
+
+fit2 <- lm(mpg ~ wt+am, mc)
+summary(fit2)
+par(mfrow=c(2,2))
+plot(fit2)
+confint(fit2)
+
+fit3 <- lm(mpg ~ wt*am, mc)
+summary(fit3)
+par(mfrow=c(2,2))
+plot(fit3)
+confint(fit3)
+
+fit4 <- lm(mpg ~ disp+am, mc)
+summary(fit4)
+par(mfrow=c(2,2))
+plot(fit4)
+confint(fit4)
+
+fit5 <- lm(mpg ~ disp*am, mc)
+summary(fit5)
+par(mfrow=c(2,2))
+plot(fit5)
+confint(fit5)
+
+fit6 <- lm(mpg ~ cyl+am, mc)
+summary(fit6)
+par(mfrow=c(2,2))
+plot(fit6)
+confint(fit6)
+
+#fit7 <- lm(mpg ~ cyl*am, mc)
+#summary(fit7)
+#par(mfrow=c(2,2))
+#plot(fit7)
+#confint(fit7)
+
+fit8 <- lm(mpg ~ disp, mc)
+summary(fit8)
+par(mfrow=c(2,2))
+plot(fit8)
+confint(fit8)
+
+fit9 <- lm(mpg ~ disp*cyl, mc)
+summary(fit9)
+par(mfrow=c(2,2))
+plot(fit9)
+confint(fit9)
+
+fit10 <- lm(mpg ~ wt, mc)
+summary(fit10)
+par(mfrow=c(2,2))
+plot(fit10)
+confint(fit10)
+
+#fit11 <- lm(mpg ~ wt+disp*cyl, mc)
+#summary(fit11)
+#par(mfrow=c(2,2))
+#plot(fit11)
+#confint(fit11)
+
+fit12 <- lm(mpg ~ hp+am, mc)
+summary(fit12)
+par(mfrow=c(2,2))
+plot(fit12)
+confint(fit12)
+
+#fit13 <- lm(mpg ~ hp*am, mc)
+#summary(fit13)
+#par(mfrow=c(2,2))
+#plot(fit13)
+#confint(fit13)
+
+fit14 <- lm(mpg ~ hp, mc)
+summary(fit14)
+par(mfrow=c(2,2))
+plot(fit14)
+confint(fit14)
+
+fit15 <- lm(mpg ~ wt+disp, mc)
+summary(fit15)
+par(mfrow=c(2,2))
+plot(fit15)
+confint(fit15)
+
+par(mfrow=c(1,1))
+with(mc, plot(wt, mpg, col=am))
+abline(fit$coef[1], fit$coef[2], col=1)
+abline(fit$coef[1]+fit$coef[3], fit$coef[2]+fit$coef[4], col=2)
 
 par(mfrow=c(2,3))
 with(mtcars, {
